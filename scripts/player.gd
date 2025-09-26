ebdcf456ca9bbc085819c51c5a877988e44bfee3
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-26 20:08:57 +0600

    Jumping improvements (more like degrdations)

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,30 @@
 extends CharacterBody2D
+
 @export var speed = 200
 @export var jump_speed = 400
-@export var gravity = 0
+@export var gravity = 1000
 
 func _ready() -> void:
-	$AnimatedSprite2D.play() # Replace with function body.
+	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
+	var input_x = Input.get_axis("move_left", "move_right")
+	velocity.x = input_x * speed
+
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		
-	var input_x = Input.get_axis("move_left", "move_right")
-	velocity.x = input_x
-	if velocity.length() != 0:
+
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_speed
+
+	if not is_on_floor():
+		$AnimatedSprite2D.animation = "jump"
+	elif velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
-		velocity = velocity * speed
-		if velocity.x > 0:
-			$AnimatedSprite2D.flip_h = false
-		else:
-			$AnimatedSprite2D.flip_h = true
 	else:
 		$AnimatedSprite2D.animation = "idle"
-		
-	if Input.is_action_just_pressed("jump"):
-		$AnimatedSprite2D.animation = "jump"
-		velocity.y = -jump_speed
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
 	move_and_slide()
