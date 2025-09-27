a164a13c8dfb026d0dd307a02a5271b50efe3fc5
Shaiara Noshin <shaiara.noshin@g.bracu.ac.bd>
2025-09-27 18:50:27 +0600

    Implement double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 200
 @export var jump_speed = 400
 @export var gravity = 1000
+var jump_count = 0
 
 func _ready() -> void:
 	$AnimatedSprite2D.play()
@@ -10,9 +11,16 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var input_x = Input.get_axis("move_left", "move_right")
 	velocity.x = input_x * speed
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	
+	var can_jump = jump_count < 2 and velocity.y <= 0
+	
+	if is_on_floor():
+		jump_count = 0
+		velocity.y = 0
+	
+	if Input.is_action_just_pressed("jump") and can_jump:
 		velocity.y = -jump_speed
+		jump_count += 1
 
 	if not is_on_floor():
 		velocity.y += gravity * delta
