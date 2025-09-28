44c5cd2165aa1e56a1c3ae649cc235a6a1d7348d
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-29 05:39:57 +0600

    Crate durability update

--- assets/entities/basic_enemy/basic_enemy.gd
+++ assets/entities/basic_enemy/basic_enemy.gd
@@ -1,10 +1,8 @@
 extends CharacterBody2D
 var health = 2
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("shootable")
-
 
 func hit():
 	health -= 1
--- assets/environment/crate/crate.gd
+++ assets/environment/crate/crate.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 class_name Crate
-
+var health = 2
 func _ready():
 	add_to_group("shootable")
 
 func hit():
-	queue_free()
+	health -= 1
+	if health <= 0: queue_free()
