9c5e8bec34f3f131862b00bdbf6b22e44aea4e87
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-28 19:16:22 +0600

    Improve crate logic

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,14 +4,13 @@ extends CharacterBody2D
 @export var jump_speed = 400
 @export var gravity = 1000
 var jump_count = 0
+@export var PUSH_STRENGTH = 400
 
 func _ready() -> void:
 	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
 	var input_x = Input.get_axis("move_left", "move_right")
-	velocity.x = input_x * speed
-	
 	var can_jump = jump_count < 2
 	
 	if is_on_floor():
@@ -33,13 +32,20 @@ func _physics_process(delta: float) -> void:
 	else:
 		$AnimatedSprite2D.animation = "idle"
 
+	velocity.x = input_x * speed
+
 	if velocity.x != 0:
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	
 	move_and_slide()
-	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(i)
-		var body = collision.get_collider()
-		if body is RigidBody2D:
-			print("pushed crate")
-			body.apply_central_impulse(velocity * delta * 50)
+	
+	if input_x != 0:
+		for i in range(get_slide_collision_count()):
+			var collision = get_slide_collision(i)
+			var body = collision.get_collider()
+			
+			if body is Crate:
+				var normal = collision.get_normal()
+			
+				if sign(input_x) == -sign(normal.x):
+					body.linear_velocity.x = input_x * PUSH_STRENGTH
