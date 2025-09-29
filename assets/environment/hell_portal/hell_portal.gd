52d7d8bea75cf4fb72f2a6274d82055dfda925d5
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-29 23:31:22 +0600

    Implement hell portal logic

--- assets/environment/hell_portal/hell_portal.gd
+++ assets/environment/hell_portal/hell_portal.gd
@@ -2,3 +2,9 @@ extends Area2D
 
 func _ready() -> void:
 	$AnimatedSprite2D.play()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		await get_tree().create_timer(1).timeout
+		get_tree().change_scene_to_file("res://assets/levels/level_2/level_2.tscn")
