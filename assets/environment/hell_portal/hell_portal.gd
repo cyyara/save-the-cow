37ddeee212d18190a9d5bfae593d775b20695a49
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-30 06:49:31 +0600

    Improve hellportal transition time

--- assets/environment/hell_portal/hell_portal.gd
+++ assets/environment/hell_portal/hell_portal.gd
@@ -6,5 +6,5 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.25).timeout
 		get_tree().change_scene_to_file("res://assets/levels/level_2/level_2.tscn")
