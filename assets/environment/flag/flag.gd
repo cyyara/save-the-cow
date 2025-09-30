64d96697bd056fb9f78f8894e7e4fd953c7fcbde
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-30 10:41:51 +0600

    Update flag animations

--- assets/environment/flag/flag.gd
+++ assets/environment/flag/flag.gd
@@ -1,17 +1,17 @@
 extends Area2D
+@export_enum("move", "jump", "shoot", "off") var message:String = "off"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.animation = "off"
-	$AnimatedSprite2D.play()
-	$Panel.visible = false
-	
+	$AnimatedSprite2D.play("off")
+	$Message.hide()
 func _on_body_entered(body):
 	if body is Player:
 		$AnimatedSprite2D.animation = "on"
-		$Panel.visible = true
-
+		$Message.show()
+		$Message.play(message)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body is Player:
-		$Panel.visible = false
+		$Message.hide()
