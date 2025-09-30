cb3c29d2b47088a5de6ca4c0a7a6be845e7d5241
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-09-30 11:10:25 +0600

    Tweak bullet despawn logic for game balance

--- assets/items/bullet/bullet.gd
+++ assets/items/bullet/bullet.gd
@@ -1,13 +1,11 @@
 extends Area2D
 var direction
 var speed = 1500
-var lifetime = 1
-func _ready():
-	await get_tree().create_timer(lifetime).timeout
-	queue_free()
 
 func _physics_process(delta: float) -> void:
 	position += direction * delta * speed
+	if not $VisibleOnScreenNotifier2D.is_on_screen():
+		queue_free()
 	
 
 func _on_body_entered(body: Node2D) -> void:
