253cfe1a0bef84f13706ab976b9fc7b86ddf39c1
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-10-15 19:15:53 +0600

    Revamp gameover menu, minor ui tweaks

--- assets/ui/game_over/game_over_menu.gd
+++ assets/ui/game_over/game_over_menu.gd
@@ -1,17 +1,12 @@
-extends Control
-
-@onready var anim_sprite = $GameOverSprite
-@onready var retry_button = $HBoxContainer/retry 
-@onready var exit_button = $HBoxContainer/exit
+extends MarginContainer
 
 func _ready():
-	anim_sprite.play("default")
-	retry_button.pressed.connect(_on_retry_pressed)
-	exit_button.pressed.connect(_on_exit_pressed)  
+	$Text.play("default")
 
-func _on_retry_pressed():
+func _on_retry_level_button_pressed() -> void:
 	var last = get_tree().get_meta("last_scene")
 	get_tree().change_scene_to_file(last)
 
-func _on_exit_pressed():
+
+func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/ui/menu/main_menu.tscn")
