1d2d0efb6626b265c68f1fcefaa3d7f5b20a17eb
Khan Mohammed Fahad <khan.mohammed.fahad@g.bracu.ac.bd>
2025-10-15 20:01:34 +0600

    Revamp start screen

--- assets/ui/menu/main_menu.gd
+++ assets/ui/menu/main_menu.gd
@@ -1,10 +1,4 @@
 extends Control
 
-
-func _on_startgame_pressed():
+func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://assets/levels/level_1/level_1.tscn")
-	
-
-
-func _on_exit_pressed():
-	get_tree().quit()
